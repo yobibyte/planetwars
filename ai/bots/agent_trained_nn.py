8b54d29c8f26250e8fd433cec2f3136991ea4f9f
Alex J. Champandard <alexjc@aigamedev.com>
2015-01-28 21:39:35 +0100

    Reworked the bot to use the class API rather than functional.

--- ai/bots/agent_trained_nn.py
+++ ai/bots/agent_trained_nn.py
@@ -19,145 +19,147 @@ def dist(src, dst):
   return d
 
 
-@planetwars_ai("AgentTrainedNN")
-def agent_trained_nn(turn, pid, planets, fleets):
+@planetwars_class
+class DeepBot(obect):
 
-  assert pid == 1 or pid == 2, "what?"
+  def __call__(turn, pid, planets, fleets):
 
-  my_planets, other_planets = partition(lambda x: x.owner == pid, planets)
-  your_planets, neutral_planets = partition(lambda x: x.owner != 0, other_planets)
+    assert pid == 1 or pid == 2, "what?"
 
-# create feature matrix
-# [  feature_list for move 1, ... , feature_list for move n ]
-#
-  if len(my_planets) == 0:
-    return []
+    my_planets, other_planets = partition(lambda x: x.owner == pid, planets)
+    your_planets, neutral_planets = partition(lambda x: x.owner != 0, other_planets)
 
-#  for i,p in enumerate(planets):
-#    print "PLANET: ", i, p.id, "x=", p.x, "y=", p.y, p.owner, p.ships
-  
-  my_ships_total = 0
-  your_ships_total = 0
-  neutral_ships_total = 0
-  my_growth = 0
-  your_growth = 0
+  # create feature matrix
+  # [  feature_list for move 1, ... , feature_list for move n ]
+  #
+    if len(my_planets) == 0:
+      return []
 
-  # tally ships and growth
-  
-  for p in planets:
-    if p.id == 0:
-      neutral_ships_total += p.ships
-    elif p.id == pid:
-      my_growth += p.growth
-      my_ships_total += p.ships
-    else:
-      your_ships_total += p.ships
-      your_growth += p.growth
+  #  for i,p in enumerate(planets):
+  #    print "PLANET: ", i, p.id, "x=", p.x, "y=", p.y, p.owner, p.ships
+    
+    my_ships_total = 0
+    your_ships_total = 0
+    neutral_ships_total = 0
+    my_growth = 0
+    your_growth = 0
 
-  # compute maximal distance between planets
+    # tally ships and growth
+    
+    for p in planets:
+      if p.id == 0:
+        neutral_ships_total += p.ships
+      elif p.id == pid:
+        my_growth += p.growth
+        my_ships_total += p.ships
+      else:
+        your_ships_total += p.ships
+        your_growth += p.growth
 
-  max_dist = 0
-  for src in planets:
-    for dst in planets:
-      d = dist(src, dst)
-      if d > max_dist:
-        max_dist = d
+    # compute maximal distance between planets
 
-  # incoming ship buckets
-  buckets = 10
+    max_dist = 0
+    for src in planets:
+      for dst in planets:
+        d = dist(src, dst)
+        if d > max_dist:
+          max_dist = d
+
+    # incoming ship buckets
+    buckets = 10
+          
+    # incoming ship bucket matrix
+    # incoming friendly ships count 1, incoming enemy ships count -1
+    # for each planet we tally incoming ship for time buckets 0..buckets-1
+    # where buckets refers to the maximum distance on the map
+    tally = numpy.zeros((len(planets), buckets))
+
+    for f in fleets:
+      # d = remaining distance
+      d = dist(planets[f.source], planets[f.destination]) * \
+          (f.remaining_turns/f.total_turns)
+      b = d/max_dist * buckets
+      if b >= buckets:
+        b = buckets-1
+      tally[f.destination, b] += f.ships * (1 if f.owner == pid else -1)
+    
+    all_orders = []
+    fm = []
+    
+    for src in my_planets:
+      for dst in planets:
+        if src == dst:
+          continue
+
+        fv = []
+        # planet totals
+        fv.append(src.ships)
+        fv.append(dst.ships)
+
+        # ship total
+        fv.append(my_ships_total)
+        fv.append(your_ships_total)
+        fv.append(neutral_ships_total)
+
+        # growth
+        fv.append(my_growth)
+        fv.append(your_growth)
+
+        # distance
+        d = dist(src, dst)
+        #print "PLANET DIST: ", src, dst
+        #print "DIST: ", src.id, dst.id, d
+        fv.append(d)
+
+        # I own dst planet
+        fv.append(1 if dst.id == pid else 0)
+        # you own dst planet
+        fv.append(1 if dst.id != 0 and dst.id != pid else 0)
+        # neutral owns dst planet
+        fv.append(1 if dst.id == 0 else 0)
+
+        # growth
+        fv.append(src.growth)
+        fv.append(dst.growth)
+
+        # incoming ship buckets (src)
+
+        # print "incoming src", src.id, ": ", 
+
+        for i in range(buckets):
+          fv.append(tally[src.id, i])
+          # print i, tally[src.id, i],
+        #print
         
-  # incoming ship bucket matrix
-  # incoming friendly ships count 1, incoming enemy ships count -1
-  # for each planet we tally incoming ship for time buckets 0..buckets-1
-  # where buckets refers to the maximum distance on the map
-  tally = numpy.zeros((len(planets), buckets))
+        # incoming ship buckets (dst)
+        # print "incoming dst", dst.id, ": ", 
 
-  for f in fleets:
-    # d = remaining distance
-    d = dist(planets[f.source], planets[f.destination]) * \
-        (f.remaining_turns/f.total_turns)
-    b = d/max_dist * buckets
-    if b >= buckets:
-      b = buckets-1
-    tally[f.destination, b] += f.ships * (1 if f.owner == pid else -1)
-  
-  all_orders = []
-  fm = []
-  
-  for src in my_planets:
-    for dst in planets:
-      if src == dst:
-        continue
+        for i in range(buckets):
+          fv.append(tally[dst.id, i])
+          #print tally[dst.id, i],
+        #print
 
-      fv = []
-      # planet totals
-      fv.append(src.ships)
-      fv.append(dst.ships)
+        # todo: add more percentage options
+        # need to create one feature vector for each option
 
-      # ship total
-      fv.append(my_ships_total)
-      fv.append(your_ships_total)
-      fv.append(neutral_ships_total)
+        perc = 50 # ship percentage
+        
+        fv.append(perc)  
+        
+        fm.append(fv);
+        all_orders.append(Order(src, dst, src.ships*perc/100))
 
-      # growth
-      fv.append(my_growth)
-      fv.append(your_growth)
+    # intermediate reward = 0 for now      
 
-      # distance
-      d = dist(src, dst)
-      #print "PLANET DIST: ", src, dst
-      #print "DIST: ", src.id, dst.id, d
-      fv.append(d)
+    order_ids = bot_act(fm, 0)
+    # order_ids = [ 0 ]
+    
+    orders = []
+    for id in order_ids:
+      orders.append(all_orders[id])
 
-      # I own dst planet
-      fv.append(1 if dst.id == pid else 0)
-      # you own dst planet
-      fv.append(1 if dst.id != 0 and dst.id != pid else 0)
-      # neutral owns dst planet
-      fv.append(1 if dst.id == 0 else 0)
+    return orders
 
-      # growth
-      fv.append(src.growth)
-      fv.append(dst.growth)
-
-      # incoming ship buckets (src)
-
-      # print "incoming src", src.id, ": ", 
-
-      for i in range(buckets):
-        fv.append(tally[src.id, i])
-        # print i, tally[src.id, i],
-      #print
-      
-      # incoming ship buckets (dst)
-      # print "incoming dst", dst.id, ": ", 
-
-      for i in range(buckets):
-        fv.append(tally[dst.id, i])
-        #print tally[dst.id, i],
-      #print
-
-      # todo: add more percentage options
-      # need to create one feature vector for each option
-
-      perc = 50 # ship percentage
-      
-      fv.append(perc)  
-      
-      fm.append(fv);
-      all_orders.append(Order(src, dst, src.ships*perc/100))
-
-  # intermediate reward = 0 for now      
-
-  order_ids = bot_act(fm, 0)
-  # order_ids = [ 0 ]
-  
-  orders = []
-  for id in order_ids:
-    orders.append(all_orders[id])
-
-  return orders
-
-# inform learner that game ended
-def done(self, won):
-  pass
+  # inform learner that game ended
+  def done(self, won):
+    pass
