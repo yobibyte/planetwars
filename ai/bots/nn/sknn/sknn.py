5d2ffe1201e865ca70473cffb32215951de120c7
ssamot <spyridon.samothrakis@gmail.com>
2015-01-29 12:12:28 +0000

    100 minibatches

--- ai/bots/nn/sknn/sknn.py
+++ ai/bots/nn/sknn/sknn.py
@@ -133,7 +133,7 @@ class sknn():
         inputs = self.mlp.get_input_space().make_theano_batch()
         self.f = theano.function([inputs], self.mlp.fprop(inputs))
 
-    def fit(self, X, y, epochs=1):
+    def fit(self, X, y, epochs=100):
         """
         :param X: Training data
         :param y:
