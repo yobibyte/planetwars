c70ec6a3d6c70232330fff9ba9029549790b9e43
ethihsy <yshihte@gmail.com>
2016-09-24 00:14:49 +0200

    add reward for exp 2

--- planetwars/game.py
+++ planetwars/game.py
@@ -188,8 +188,8 @@ class PlanetWars:
         reward_g=0
         reward_s=0
         
-        # reward_g = sum([plt.growth for plt in planets if plt.owner==self.DQN_id])
-        # reward_g -= sum([plt.growth for plt in planets if plt.owner!=self.DQN_id and plt.owner!=0])
+        reward_g = sum([plt.growth for plt in planets if plt.owner==self.DQN_id])
+        reward_g -= sum([plt.growth for plt in planets if plt.owner!=self.DQN_id and plt.owner!=0])
         
         reward_ps = sum([plt.ships for plt in planets if plt.owner==self.DQN_id])
         reward_ps -= sum([plt.ships for plt in planets if plt.owner!=self.DQN_id and plt.owner!=0])
@@ -197,10 +197,10 @@ class PlanetWars:
         reward_fs = sum([f.ships for f in fleets if f.owner==self.DQN_id])
         reward_fs -= sum([f.ships for f in fleets if f.owner!=self.DQN_id])
 
-        # alpha = (1-self.turn/200)*reward_ps*0.3
+        alpha = (1-self.turn/200)*reward_ps*0.2
 
-        # return reward_g*alpha+reward_ps+reward_fs
-        return float(reward_ps+reward_fs)
+        return reward_g*alpha+reward_ps+reward_fs
+        # return float(reward_ps+reward_fs)
 
     def issue_order(self, player, order):
         if order.source.owner != player:
